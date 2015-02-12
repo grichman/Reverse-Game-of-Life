cbbc99f3417290f0e1a511e0494d6467f86b6c7c
Gabe <grichman@oberlin.edu>
2015-02-12 17:41:32 -0500

    added the initial scripts

---
+++ Assets/Resources/Scripts/InputReader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputReader : MonoBehaviour {
+	GameObject[,] tiles;
+	// Use this for initialization
+	void Start () {
+		//tiles = new GameObject[100,100];
+		GameObject obj = new GameObject();
+		InputTile tile = obj.AddComponent<InputTile>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ Assets/Resources/Scripts/InputTile.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputTile : MonoBehaviour {
+	private const float outlineWidth = 0.04f;
+	GameObject outlineObject;
+	GameObject modelObject;
+	// Use this for initialization
+	void Start () {
+		modelObject = GameObject.CreatePrimitive(PrimitiveType.Quad);
+		modelObject.renderer.material.color = Color.white;
+		modelObject.transform.localScale *= (1 - outlineWidth);
+		modelObject.transform.position = this.transform.position;
+		modelObject.transform.parent = this.transform;
+
+		outlineObject = GameObject.CreatePrimitive(PrimitiveType.Quad);
+		outlineObject.renderer.material.color = Color.black;
+		outlineObject.transform.position = this.transform.position;
+		outlineObject.transform.parent = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
