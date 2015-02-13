f7ffe96d2487c3d79a61ebdfa263e9c725abfbb6
Gabe <grichman@oberlin.edu>
2015-02-13 01:25:30 -0500

    small for-convenience coroutine for running the game

--- Assets/Resources/Scripts/LifeRun.cs
+++ Assets/Resources/Scripts/LifeRun.cs
@@ -2,17 +2,33 @@
 using System.Collections;
 
 public class LifeRun : MonoBehaviour {
+	private const float stepTime = 0.1f;
 	Board board;
 	// Use this for initialization
 	void Start () {
 		board = new Board("level");
 		Camera.main.transform.position = new Vector3(10,10,-10);
 	}
-	
+
+	bool isRunning = false;
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyUp(KeyCode.I)){
 			print("HI");
+			board.runOneStep();
+		}
+		if (Input.GetKeyUp(KeyCode.Return)){
+			if (!isRunning){
+				StartCoroutine("runLife");
+			}else{
+				StopCoroutine("runLife");
+			}
+		}
+	}
+
+	IEnumerator runLife(float stepTime = LifeRun.stepTime){
+		while(true){
+			yield return new WaitForSeconds(stepTime);
 			board.runOneStep();
 		}
 	}
